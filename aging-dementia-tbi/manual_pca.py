eeffb3ab2779f553bb85ccb3ebb523df9dbd1412
jeremygrace <jeremy.grace@outlook.com>
2018-01-18 18:38:46 -0800

    Add name/main for manual matrix entry

--- aging-dementia-tbi/manual_pca.py
+++ aging-dementia-tbi/manual_pca.py
@@ -22,13 +22,16 @@ def perform_pca(X):
     # retrieve the eignvalues and eignvectors
     eig_vals, eig_vecs = np.linalg.eig(C)
     # pair up the eignvectors with corresponding eignvalues
-    eigens = [(np.abs(eig_vals[i]), eig_vecs[:,i]) for i in range(len(eig_vals))]
-    eigens.sort(reverse=True)
+    eig = [(np.abs(eig_vals[i]), eig_vecs[:, i]) for i in range(len(eig_vals))]
+    eig.sort(reverse=True)
     # dimensional reduce by a built projection matrix and
     # transforming the initial data into a new v-dimensional subspace
-    Y = np.hstack((eigens[0][1][:, np.newaxis], eigens[1][1][:, np.newaxis]))
+    Y = np.hstack((eig[0][1][:, np.newaxis], eig[1][1][:, np.newaxis]))
     X_pca = np.dot(X_norm, Y)
 
     return X_pca
 
-perform_pca(X)
+
+if __name__ == "__main__":
+    X = input("Enter data matrix for PCA: ")
+    perform_pca(X)
